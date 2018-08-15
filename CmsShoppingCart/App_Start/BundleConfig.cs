85e6cfbe30748616f5d885f7a759630c3321f961
naveenan18 <naveen.yadne@gmail.com>
2018-08-15 10:05:31 +0530

    Admin Dashboard
    Admin Dashboard Setup

--- CmsShoppingCart/App_Start/BundleConfig.cs
+++ CmsShoppingCart/App_Start/BundleConfig.cs
@@ -8,13 +8,16 @@ namespace CmsShoppingCart
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/Scripts").Include(
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
